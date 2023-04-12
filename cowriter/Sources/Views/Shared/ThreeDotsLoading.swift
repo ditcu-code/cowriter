ea8258cc24ea5bed9efea2c94d221d00c460983d
ditcu-code <63937626+ditcu-code@users.noreply.github.com>
2023-04-12 17:24:10 +0700

    add three dots loading

---
+++ cowriter/Sources/Views/Shared/ThreeDotsLoading.swift
@@ -0,0 +1,41 @@
+//
+//  ThreeDotsLoading.swift
+//  cowriter
+//
+//  Created by Aditya Cahyo on 12/04/23.
+//
+
+import SwiftUI
+
+struct ThreeDotsLoading: View {
+    @State var loading = false
+    
+    var body: some View {
+        HStack(spacing: 20) {
+            Circle()
+                .fill(Color.orange)
+                .frame(width: 10, height: 10)
+                .scaleEffect(loading ? 1.5 : 0.5)
+                .animation(.easeInOut(duration: 0.8).repeatForever(autoreverses: true), value: loading)
+            Circle()
+                .fill(Color.orange)
+                .frame(width: 10, height: 10)
+                .scaleEffect(loading ? 1.5 : 0.5)
+                .animation(.easeInOut(duration: 0.8).repeatForever(autoreverses: true).delay(0.2), value: loading)
+            Circle()
+                .fill(Color.orange)
+                .frame(width: 10, height: 10)
+                .scaleEffect(loading ? 1.5 : 0.5)
+                .animation(.easeInOut(duration: 0.8).repeatForever(autoreverses: true).delay(0.4), value: loading)
+        }
+        .onAppear() {
+            self.loading = true
+        }
+    }
+}
+
+struct ThreeDotsLoading_Previews: PreviewProvider {
+    static var previews: some View {
+        ThreeDotsLoading()
+    }
+}
