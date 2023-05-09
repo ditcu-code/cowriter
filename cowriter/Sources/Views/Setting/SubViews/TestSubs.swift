b3d1bd6080a4b7cec13d189d10de686de0d7e919
ditcu-code <work.adityac@gmail.com>
2023-05-09 16:39:46 +0700

    Sharing active purchases with extensions - step 7 (2)

--- cowriter/Sources/Views/Setting/SubViews/TestSubs.swift
+++ cowriter/Sources/Views/Setting/SubViews/TestSubs.swift
@@ -8,10 +8,19 @@
 import SwiftUI
 
 struct TestSubs: View {
-    @StateObject private var purchaseManager = PurchaseManager()
+    @StateObject private var entitlementManager: EntitlementManager
+    @StateObject private var purchaseManager: PurchaseManager
+    
+    init() {
+        let entitlementManager = EntitlementManager()
+        let purchaseManager = PurchaseManager(entitlementManager: entitlementManager)
+        self._entitlementManager = StateObject(wrappedValue: entitlementManager)
+        self._purchaseManager = StateObject(wrappedValue: purchaseManager)
+    }
     
     var body: some View {
         TestStoreKit()
+            .environmentObject(entitlementManager)
             .environmentObject(purchaseManager)
             .task {
                 await purchaseManager.updatePurchasedProducts()
