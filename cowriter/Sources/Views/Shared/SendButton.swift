fde01eb03c16b8f3dd08c05c4ae15a313a86b363
ditcu-code <work.adityac@gmail.com>
2023-07-13 20:14:18 +0700

    fix size on prompter

--- cowriter/Sources/Views/Shared/SendButton.swift
+++ cowriter/Sources/Views/Shared/SendButton.swift
@@ -12,29 +12,28 @@ struct SendButton: View {
     let action: () -> Void
 
     var body: some View {
-        Button(action:
-            action,
-            label: {
-                Circle()
-                    .overlay {
-                        if loading {
-                            ProgressView()
-                                .progressViewStyle(CircularProgressViewStyle(tint: .white))
-                                .scaleEffect(0.75)
-                                .padding(.horizontal, 10)
-                        } else {
-                            Image(systemName: "paperplane.fill")
-                                .font(.footnote)
-                                .foregroundColor(.white)
-                        }
-                    }.frame(width: 36)
-            })
+        Button(action: action) {
+            Circle()
+                .overlay {
+                    if loading {
+                        ProgressView()
+                            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+                            .scaleEffect(0.75)
+                            .padding(.horizontal, 10)
+                    } else {
+                        Image(systemName: "paperplane.fill")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    }
+                }
+                .frame(width: 32)
+        }
     }
 }
 
 struct SendButton_Previews: PreviewProvider {
     static var previews: some View {
-        SendButton(loading: true) {
+        SendButton(loading: false) {
             print("clicked")
         }
     }
